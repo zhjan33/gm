529813124dfe6f374849f5a3f1a7b0d8ac3b68c4
zhjan33 <62002124+zhjan33@users.noreply.github.com>
2022-07-09 16:29:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'sg2.31vpn.com'
+      url.hostname = 'sg1.31vpn.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
